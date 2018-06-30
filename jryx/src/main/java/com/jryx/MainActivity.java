ee56e675ff164fef8b7394160b7e87e7f0ba3a91
author <412700845@qq.com>
2018-06-30 15:12:40 +0800

    "jjjj"

--- jryx/src/main/java/com/jryx/MainActivity.java
+++ jryx/src/main/java/com/jryx/MainActivity.java
@@ -2,12 +2,15 @@ package com.jryx;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.jryx.base.BaseActivity;
 
@@ -18,12 +21,42 @@ public class MainActivity extends BaseActivity {
 
     @BindView(R.id.viewPager)
     ViewPager viewPager;
-    @BindView(R.id.bottom_navigation)
-    BottomNavigationView bottomNavigation;
     @BindView(R.id.nav)
     NavigationView navigationView;
     @BindView(R.id.activity_view)
     DrawerLayout drawerLayout;
+    @BindView(R.id.titleBar)
+    LinearLayout titleBar;
+    @BindView(R.id.img_main_release)
+    ImageView imgMainRelease;
+    @BindView(R.id.cl_main_release)
+    ConstraintLayout clMainRelease;
+    @BindView(R.id.img_main_home)
+    ImageView imgMainHome;
+    @BindView(R.id.tv_main_home)
+    TextView tvMainHome;
+    @BindView(R.id.cl_main_home)
+    ConstraintLayout clMainHome;
+    @BindView(R.id.img_main_collection)
+    ImageView imgMainCollection;
+    @BindView(R.id.tv_main_collection)
+    TextView tvMainCollection;
+    @BindView(R.id.cl_main_collection)
+    ConstraintLayout clMainCollection;
+    @BindView(R.id.img_main_circle)
+    ImageView imgMainCircle;
+    @BindView(R.id.tv_main_circle)
+    TextView tvMainCircle;
+    @BindView(R.id.cl_main_circle)
+    ConstraintLayout clMainCircle;
+    @BindView(R.id.img_main_video)
+    ImageView imgMainVideo;
+    @BindView(R.id.tv_main_video)
+    TextView tvMainVideo;
+    @BindView(R.id.cl_main_video)
+    ConstraintLayout clMainVideo;
+    @BindView(R.id.cl_main_bottom)
+    ConstraintLayout clMainBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,4 +80,6 @@ public class MainActivity extends BaseActivity {
 
 
     }
+
+
 }
