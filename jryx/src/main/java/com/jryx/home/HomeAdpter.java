19112a1ffbd21b8c905a2d89f0d2fc986ae24a08
author <412700845@qq.com>
2018-07-01 11:05:48 +0800

    7.1

---
+++ jryx/src/main/java/com/jryx/home/HomeAdpter.java
@@ -0,0 +1,29 @@
+package com.jryx.home;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class HomeAdpter extends FragmentPagerAdapter {
+
+    public HomeAdpter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return super.getPageTitle(position);
+    }
+}
