b0070d79d73fcf8ac00abd1c529fe3a87a7d8f72
sunshaobei <sunshaobei@foxmail.com>
2018-07-01 11:08:19 +0800

    Merge branch 'master' of https://github.com/sunshaobei/jiriyixing into develop_ssb
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- jryx/src/main/java/com/jryx/login/LoginActivity.java
+++ jryx/src/main/java/com/jryx/login/LoginActivity.java
@@ -1,4 +1,35 @@
 package com.jryx.login;
 
-public class LoginActivity {
+import android.view.View;
+
+import com.jryx.R;
+import com.jryx.base.JryxActivity;
+
+public class LoginActivity extends JryxActivity {
+    @Override
+    public int getLayoutID() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    public void finish(View view) {
+        onBackPressed();
+    }
+    public void register(View view) {
+        onBackPressed();
+    }
 }
--- jryx/src/main/java/com/jryx/main/MainActivity.java
+++ jryx/src/main/java/com/jryx/main/MainActivity.java
@@ -1,61 +1,48 @@
 package com.jryx.main;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
 
 import com.jryx.R;
 import com.jryx.base.BaseActivity;
+import com.jryx.lib.utils.StatusBarUtil;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
-import butterknife.OnClick;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements StatubarDarkService, ViewPager.OnPageChangeListener {
 
 
     @BindView(R.id.viewPager)
     ViewPager viewPager;
     @BindView(R.id.activity_view)
     DrawerLayout drawerLayout;
-    @BindView(R.id.titleBar)
-    LinearLayout titleBar;
-    @BindView(R.id.img_main_release)
-    ImageView imgMainRelease;
     @BindView(R.id.cl_main_release)
     ConstraintLayout clMainRelease;
-    @BindView(R.id.img_main_home)
-    ImageView imgMainHome;
-    @BindView(R.id.tv_main_home)
-    TextView tvMainHome;
     @BindView(R.id.cl_main_home)
     ConstraintLayout clMainHome;
-    @BindView(R.id.img_main_collection)
-    ImageView imgMainCollection;
-    @BindView(R.id.tv_main_collection)
-    TextView tvMainCollection;
     @BindView(R.id.cl_main_collection)
     ConstraintLayout clMainCollection;
-    @BindView(R.id.img_main_circle)
-    ImageView imgMainCircle;
-    @BindView(R.id.tv_main_circle)
-    TextView tvMainCircle;
     @BindView(R.id.cl_main_circle)
     ConstraintLayout clMainCircle;
-    @BindView(R.id.img_main_video)
-    ImageView imgMainVideo;
-    @BindView(R.id.tv_main_video)
-    TextView tvMainVideo;
     @BindView(R.id.cl_main_video)
     ConstraintLayout clMainVideo;
+
+
+    private boolean isStatubarDark;
 
     private List<ViewGroup> menuList = new ArrayList<>();
 
@@ -64,31 +51,78 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initListener();
     }
 
     protected void initView() {
-        initBottom();
-    }
-
-    private void initBottom() {
         menuList.add(clMainHome);
         menuList.add(clMainCollection);
         menuList.add(clMainCircle);
         menuList.add(clMainVideo);
-        for (ViewGroup viewGroup : menuList) {
-            viewGroup.setOnClickListener(this::update);
+        for (int i = 0; i < menuList.size(); i++) {
+            int tag = i;
+            menuList.get(i).setOnClickListener(v -> update(tag));
         }
-        update(clMainHome);
+        viewPager.setAdapter(new MainVPAdapter(getSupportFragmentManager()));
+        update(0);
     }
 
-    private void update(View v) {
-        for (ViewGroup viewGroup : menuList) {
-            if (viewGroup.equals(v)) {
-                viewGroup.setSelected(true);
+    private void initListener() {
+        viewPager.addOnPageChangeListener(this);
+        clMainRelease.setOnClickListener(this::publish);
+    }
+
+    private void update(int position) {
+        viewPager.setCurrentItem(position);
+        for (int i = 0; i < menuList.size(); i++) {
+            if (i == position) {
+                menuList.get(i).setSelected(true);
+                for (int j = 0; j < menuList.get(i).getChildCount(); j++) {
+                    tabAnimation(menuList.get(i).getChildAt(j));
+                }
             } else {
-                viewGroup.setSelected(false);
+                menuList.get(i).setSelected(false);
             }
         }
+
+        if (position == 0) {
+            if (isStatubarDark) {
+                isStatubarDark = false;
+                dark(false);
+            }
+
+        } else {
+            if (!isStatubarDark) {
+                isStatubarDark = true;
+                dark(true);
+            }
+        }
+    }
+
+    private void publish(View view) {
+        //TODO 发布
+    }
+
+
+    /******tab animation\*********/
+    private Map<View, AnimationSet> animationMap = new HashMap<>();
+
+    private void tabAnimation(View view) {
+        AnimationSet animationSet = animationMap.get(view);
+        if (animationSet == null) {
+            animationSet = new AnimationSet(true);
+            ScaleAnimation scaleAnimation = new ScaleAnimation(
+                    0.8f, 1f, 0.8f, 1f,
+                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f
+            );
+            scaleAnimation.setDuration(200);
+            AlphaAnimation alphaAnimation = new AlphaAnimation(0.5f, 1);
+            alphaAnimation.setDuration(200);
+            animationSet.addAnimation(scaleAnimation);
+            animationSet.addAnimation(alphaAnimation);
+            animationMap.put(view, animationSet);
+        }
+        view.startAnimation(animationSet);
     }
 
     @Override
@@ -97,4 +131,23 @@ public class MainActivity extends BaseActivity {
     }
 
 
+    @Override
+    public void dark(boolean isDark) {
+        StatusBarUtil.darkMode(this, Color.TRANSPARENT, 0.2f, isDark);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        update(position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
 }
